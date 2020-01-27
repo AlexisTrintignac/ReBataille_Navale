a6734f2cbf7711affdafc67027459eaf4c7a4288
Alexis Trintignac <alexis.trintignac@gmail.com>
2020-01-27 15:01:53 +0100

    add class

--- src/Direction.java
+++ src/Direction.java
@@ -1,4 +1,3 @@
-package PACKAGE_NAME;
-
 public enum Direction {
+    NORD,SUD,EST,OUEST;
 }
--- src/ShipModel.java
+++ src/ShipModel.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public enum ShipModel {
+    PORTE_AVION(5), CROISEUR(4), TORPILLEUR(3),CONTRE_TORPILLEUR(3),SOUS_MARIN(2);
+
+    private final int taille;
+
+    private ShipModel(int taille){
+        this.taille = taille;
+    }
+
+    public int getSize(){
+        return this.taille;
+    }
 }
